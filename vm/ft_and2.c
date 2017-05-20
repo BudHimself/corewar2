92d38199b494650cee88d3a661fc222467fca84f
snk11 <sul.yusof@gmail.com>
2017-05-20 16:21:28 +0200

    r

--- vm/ft_and2.c
+++ vm/ft_and2.c
@@ -1,88 +1,78 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sub2.c                                          :+:      :+:    :+:   */
+/*   ft_and2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/20 15:16:59 by syusof            #+#    #+#             */
-/*   Updated: 2017/05/20 16:12:14 by syusof           ###   ########.fr       */
+/*   Created: 2017/05/20 16:02:20 by syusof            #+#    #+#             */
+/*   Updated: 2017/05/20 16:18:25 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tyassine.h"
 
-static unsigned char	*ft_sub2_p1(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
+static unsigned char	*ft_and2_p1(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
 {
-	unsigned int	p;
 	unsigned char	*sc;
-	unsigned int k;
+	unsigned int	k;
 
 	k = (i >= j) ? i : j;
 	sc = (unsigned char*)malloc(sizeof(unsigned char) * k);
-	p = 0;
 	while (j > 0)
 	{
-		sc[i - 1] = sa[i - 1] - (sb[j - 1] + p);
-		p = (sa[i - 1] < (sb[j - 1] + p)) ? 1 : 0;
+		sc[i - 1] = sa[i - 1] & sb[j - 1];
 		i--;
 		j--;
 	}
 	while (i > 0)
 	{
-		sc[i - 1] = sa[i - 1] - p;
-		p = (sa[i - 1] < p) ? 1 : 0;
+		sc[i - 1] = 0;
 		i--;
 	}
 	return (sc);
+
 }
 
-static unsigned char	*ft_sub2_p2(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
+static unsigned char	*ft_and2_p2(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
 {
-	unsigned int	p;
 	unsigned char	*sc;
-	unsigned int k;
+	unsigned int	k;
 
 	k = (i >= j) ? i : j;
 	sc = (unsigned char*)malloc(sizeof(unsigned char) * k);
-	p = 0;
 	while (i > 0)
 	{
-		sc[j - 1] = sa[i - 1] - (sb[j - 1] + p);
-		p = (sa[i - 1] < (sb[j - 1] + p)) ? 1 : 0;
+		sc[j - 1] = sa[i - 1] & sb[j - 1];
 		i--;
 		j--;
 	}
 	while (j > 0)
 	{
-		sc[j - 1] = - (sb[j - 1] + p);
-		p = (0 < (sb[j - 1] + p)) ? 1 : 0;
+		sc[j - 1] = 0;
 		j--;
 	}
 	return (sc);
 }
 
-unsigned char	*ft_sub2(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
+unsigned char			*ft_and2(unsigned char *sa, unsigned char *sb, unsigned int i, unsigned int j)
 {
-	unsigned int	p;
 	unsigned char	*sc;
-	unsigned int k;
+	unsigned int	k;
 
 	if (i == j)
 	{
 		k = (i >= j) ? i : j;
 		sc = (unsigned char*)malloc(sizeof(unsigned char) * k);
-		p = 0;
 		while (i > 0)
 		{
-			sc[i - 1] = sa[i - 1] - (sb[i - 1] + p);
-			p = (sa[i - 1] < (sb[i - 1] + p)) ? 1 : 0;
+			sc[i - 1] = sa[i - 1] & sb[i - 1];
 			i--;
 		}
 	}
 	else if (i > j)
-		sc = ft_sub2_p1(sa, sb, i, j);
+		sc = ft_and2_p1(sa, sb, i, j);
 	else if (i < j)
-		sc = ft_sub2_p2(sa, sb, i, j);
+		sc = ft_and2_p2(sa, sb, i, j);
 	return (sc);
 }
