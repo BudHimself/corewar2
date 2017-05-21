b5ba42928c95fcffa6819310664e51cec0f9b58c
snk11 <sul.yusof@gmail.com>
2017-05-21 19:13:31 +0200

    delete

--- annotation/test.sh
+++
@@ -1,9 +0,0 @@
-
-file=car
-arg1=champions/$file.s
-arg2=champions/$file.cor
-
-rm test1 test2
-make && ./asm_proto "$arg1" && hexdump "$arg2" > test1
-./asm "$arg1" && hexdump "$arg2" > test2
-diff -u test1 test2
--- champions/ex.s
+++
@@ -1,8 +0,0 @@
-.name "zork"
-.comment "just a basic living prog"
-
-labeliwi: 	sti r1,%:live,%1
-	and	r1,%0,r1
-
-live:	live %:live
-		zjmp %:live
